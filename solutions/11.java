348b7bc957d1ecb585a495bc3899760336acc70a
amarpsp10 <amarpsp10@gmail.com>
2021-04-13 23:40:19 -0700

    11 worthy matrix complete solution added

--- solutions/11.java
+++ solutions/11.java
@@ -1,59 +1,109 @@
-/* package codechef; // don't place package name! */
-
+//https://www.codechef.com/APRIL21C/problems/KAVGMAT
+import java.io.*;
 import java.util.*;
-import java.lang.*;
 import java.io.*;
 
-/* Name of the class has to be "Main" only if the class is public. */
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-	   Scanner sc = new Scanner(System.in);
-	   int t = sc.nextInt();
-	   while(t-->0){
-	       int n = sc.nextInt();
-	       int m = sc.nextInt();
-	       int k = sc.nextInt();
-	       int count = 0;
-	       int a[][] = new int[n][m];
-	       long asum[][] = new long[n][m];
-	       
-	       for(int i = 0; i<n; i++){
-	           for(int j = 0; j<m; j++){
-	               a[i][j] = sc.nextInt();
-	               
-	               asum[i][j] = a[i][j];
-	               if(j>0) asum[i][j]+= asum[i][j-1];
-	               if(i>0) asum[i][j]+= asum[i-1][j];
-	               if(i>0&&j>0) asum[i][j]-= asum[i-1][j-1];
-	               
-	               if(a[i][j]>=k) count+=1;
-	           }
-	       }
-	  
-           
-	       int l = 2;
-	       
-	       while(l<=n){
-	           count += findCount(asum,n,m,k,l);
-	           l++;
-	       }
-	       System.out.println(count);
-	   }
+class Codechef {
+
+	public static void main(String[] args) {
+        FastScanner in = new FastScanner();
+        PrintWriter out = new PrintWriter(System.out);
+        int t = in.nextInt(), tt = 0;
+        while(t-->0) { 
+            n = in.nextInt(); m = in.nextInt(); long k = in.nextInt();
+            long a[][] = new long[n][m];
+            long ans = 0;
+            for(int i=0;i<n;i++){
+            	for(int j=0;j<m;j++){
+            		a[i][j] = in.nextInt();
+            		a[i][j] -= k;
+            		if(a[i][j]>=0) ans++;
+            		//out.print(a[i][j]+" ");
+            	}
+            	//out.println();
+            }
+            dp = new long[n][m];
+            for(int i=0;i<n;i++){
+            	for(int j=0;j<m;j++){
+            		dp[i][j] = a[i][j];                  
+            		if(i-1>-1) dp[i][j] += dp[i-1][j];
+            		if(j-1>-1) dp[i][j] += dp[i][j-1];
+            		if(i-1>-1&&j-1>-1) dp[i][j] -= dp[i-1][j-1]; 
+            	}
+            }
+            long cnt = 0;
+            for(int i=0;i<n;i++){
+            	for(int j=0;j<m;j++){
+            		int low = 1, mid, high = Math.min(n-1-i,m-1-j);
+            		int last = high;
+            		while(low<=high){
+            			mid = (low+high)/2;
+            			if(getSum(i,j,i+mid,j+mid)<0){
+            				low = mid+1;		
+            			}
+            			else high = mid-1;
+            		}
+            		ans += (last-low+1);
+            	}	
+            }
+            //out.println(getSum(0,0,1,1)+" "+dp[1][1]);                            
+            out.println(cnt+ans);
+    		tt++;
+    		//out.println("Case #"+tt+": "+ans);
+        }
+        out.flush();
 	}
-	public static int findCount(long[][] asum, int n, int m, int k, int l){
-	         int count = 0;
-	         for(int i = 0; i<=n-l; i++){
-	             for(int j = 0; j<=m-l; j++){
-	                 long sum = asum[i+l-1][j+l-1];
-	                 if(i>0) sum -= asum[i-1][j+l-1];
-	                 if(j>0) sum -= asum[i+l-1][j-1];
-	                 if(i>0 && j>0) sum += asum[i-1][j-1];
-	                 if(sum/(l*l)>=k) count +=1;
-	             }
-	         }
-	        
-	         return count;
+
+	static long dp[][];
+	static int n, m;
+
+	static long getSum(int i, int j, int p, int q){
+		long sum = dp[p][q];
+		if(i-1>-1) sum -= dp[i-1][q];
+		if(j-1>-1) sum -= dp[p][j-1];
+		if(i-1>-1&&j-1>-1) sum += dp[i-1][j-1];
+		return sum;
+	}
+	
+	static class FastScanner {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer("");
+		
+		String next() {
+			while(!st.hasMoreTokens())
+				try { st = new StringTokenizer(br.readLine()); }
+				catch(IOException e) {}
+			return st.nextToken();
+		}
+		
+		String nextLine(){
+			try{ return br.readLine(); } 
+			catch(IOException e) { } return "";
+		}
+		
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		
+		int[] readArray(int n) {
+			int a[] = new int[n];
+			for(int i=0;i<n;i++) a[i] = nextInt();
+			return a;
+		}
+	}
+
+	static final Random random = new Random();
+
+	static void ruffleSort(int[] a){
+		int n = a.length;
+		for(int i=0;i<n;i++){
+			int j = random.nextInt(n), temp = a[j];
+			a[j] = a[i]; a[i] = temp;
+		}
+		Arrays.sort(a); 	
 	}
 }

59bba87df679c97c17ffb54cd17ecc41682e7bd7
amarpsp10 <amarpsp10@gmail.com>
2021-04-29 05:50:14 -0700

    full solution added whrothy matrix 11 added

--- solutions/11.java
+++ solutions/11.java
@@ -1,109 +1,97 @@
 //https://www.codechef.com/APRIL21C/problems/KAVGMAT
+import java.lang.*;
 import java.io.*;
 import java.util.*;
-import java.io.*;
-
-class Codechef {
-
+public class Main {
+    public static PrintWriter out=new PrintWriter(new BufferedOutputStream(System.out));
 	public static void main(String[] args) {
-        FastScanner in = new FastScanner();
-        PrintWriter out = new PrintWriter(System.out);
-        int t = in.nextInt(), tt = 0;
-        while(t-->0) { 
-            n = in.nextInt(); m = in.nextInt(); long k = in.nextInt();
-            long a[][] = new long[n][m];
-            long ans = 0;
-            for(int i=0;i<n;i++){
-            	for(int j=0;j<m;j++){
-            		a[i][j] = in.nextInt();
-            		a[i][j] -= k;
-            		if(a[i][j]>=0) ans++;
-            		//out.print(a[i][j]+" ");
-            	}
-            	//out.println();
+		InputReader in = new InputReader(System.in);
+	int t = in.nextInt();
+        while (t-- > 0) {
+            long n = in.nextLong();
+            long m = in.nextLong();
+            long k = in.nextLong();
+            long[][] arr = new long[(int) n+1][(int) m+1];
+            for (long i = 0; i <= n; i++) {
+                for (long j = 0; j <= m; j++) {
+                    if(i==0||j==0)
+                        arr[(int)i][(int)j]=0;
+                    else
+                    arr[(int) i][(int) j] = in.nextLong();
+                }
             }
-            dp = new long[n][m];
-            for(int i=0;i<n;i++){
-            	for(int j=0;j<m;j++){
-            		dp[i][j] = a[i][j];                  
-            		if(i-1>-1) dp[i][j] += dp[i-1][j];
-            		if(j-1>-1) dp[i][j] += dp[i][j-1];
-            		if(i-1>-1&&j-1>-1) dp[i][j] -= dp[i-1][j-1]; 
-            	}
-            }
-            long cnt = 0;
-            for(int i=0;i<n;i++){
-            	for(int j=0;j<m;j++){
-            		int low = 1, mid, high = Math.min(n-1-i,m-1-j);
-            		int last = high;
-            		while(low<=high){
-            			mid = (low+high)/2;
-            			if(getSum(i,j,i+mid,j+mid)<0){
-            				low = mid+1;		
-            			}
-            			else high = mid-1;
-            		}
-            		ans += (last-low+1);
-            	}	
-            }
-            //out.println(getSum(0,0,1,1)+" "+dp[1][1]);                            
-            out.println(cnt+ans);
-    		tt++;
-    		//out.println("Case #"+tt+": "+ans);
+           out.print(CountSubMatrix(arr, n, m, k)+"\n");
+            out.flush();
+        }	
+	}
+	public static long CountSubMatrix(long[][] arr, long n, long m, long k) {
+        for (long i = 1; i <= n; i++) {
+            for (long j = 1; j <= m; j++)
+                arr[(int) i][(int) j] += arr[(int)i-1][(int)j]+arr[(int)i][(int)j-1]-arr[(int)i-1][(int)j-1];
         }
-        out.flush();
-	}
+        long c=0;
+        for(long size=1;size<=n;size++){
+            for(long i=1;i<=n-size+1;i++){
+                long l=1;
+                long h=m-size+1;
+                long mid;
+                long p=0;
+                long flag=0;
+                while(l<=h){
+                    mid=(h+l)/2;
+                    long sum=arr[(int)(i+size-1)][(int)(mid+size-1)]-arr[(int)(i+size-1)][(int)mid-1]-arr[(int)i-1][(int)(mid+size-1)]+arr[(int)(i-1)][(int)(mid-1)];
+                       if(sum>=k*size*size) {
+                           h=mid-1;
+                           p=mid;
+                           flag=1;
+                       }
+                       else
+                           l=mid+1;
+                }
+                if(flag==1)
+                    c=c+(m-size-p+2);
+            }
+        }
+        return c;
+      }
+	static class InputReader {
+		public BufferedReader reader;
+		public StringTokenizer tokenizer;
 
-	static long dp[][];
-	static int n, m;
+		public InputReader(InputStream stream) {
+			reader = new BufferedReader(new InputStreamReader(stream), 32768);
+			tokenizer = null;
+		}
 
-	static long getSum(int i, int j, int p, int q){
-		long sum = dp[p][q];
-		if(i-1>-1) sum -= dp[i-1][q];
-		if(j-1>-1) sum -= dp[p][j-1];
-		if(i-1>-1&&j-1>-1) sum += dp[i-1][j-1];
-		return sum;
-	}
-	
-	static class FastScanner {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer("");
-		
-		String next() {
-			while(!st.hasMoreTokens())
-				try { st = new StringTokenizer(br.readLine()); }
-				catch(IOException e) {}
-			return st.nextToken();
+		public String next() {
+			while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+				try {
+				    tokenizer = new StringTokenizer(reader.readLine());
+				} catch (IOException e) {
+				    throw new RuntimeException(e);
+				}
+			}
+			return tokenizer.nextToken();
 		}
-		
-		String nextLine(){
-			try{ return br.readLine(); } 
-			catch(IOException e) { } return "";
-		}
-		
-		int nextInt() {
+
+		public int nextInt() {
 			return Integer.parseInt(next());
 		}
-		
-		long nextLong() {
-			return Long.parseLong(next());
-		}
-		
-		int[] readArray(int n) {
-			int a[] = new int[n];
-			for(int i=0;i<n;i++) a[i] = nextInt();
-			return a;
-		}
-	}
-
-	static final Random random = new Random();
-
-	static void ruffleSort(int[] a){
-		int n = a.length;
-		for(int i=0;i<n;i++){
-			int j = random.nextInt(n), temp = a[j];
-			a[j] = a[i]; a[i] = temp;
-		}
-		Arrays.sort(a); 	
+		public long nextLong() { 
+       return Long.parseLong(next());
+      }
+        public double nextDouble(){
+      return Double.parseDouble(next()); 
+         }
+        public  String nextLine(){
+           String str = ""; 
+           try { 
+          str = reader.readLine(); 
+            } 
+        catch (IOException e) {
+            e.printStackTrace(); 
+             }
+         return str; 
+          }
 	}
 }
